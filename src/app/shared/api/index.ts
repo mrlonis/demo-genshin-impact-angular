b0e40691213daba563612d65ce6c41e75c45b506
Matthew L <mattlonis@gmail.com>
2022-05-04 15:31:06 -0400

    Refactored project

--- src/app/shared/api/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './artifact-set.service';
-export * from './base';
-export * from './character.service';
-export * from './element.service';
-export * from './interfaces';
-export * from './weapon.service';
